7e77ea8ee8e02617d2d866ee07e146122d2ec8e9
ropelife <anjali9jan1999@gmail.com>
2020-12-23 15:29:20 +0530

    Prescription formalities done

--- app/src/main/java/com/droiduino/companionappcourse/MainActivity.java
+++ app/src/main/java/com/droiduino/companionappcourse/MainActivity.java
@@ -288,6 +288,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button prescriptionButton = findViewById(R.id.prescriptionButton);
+        prescriptionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // This is the code to move to another screen
+                Intent intent = new Intent(MainActivity.this, Record.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 
