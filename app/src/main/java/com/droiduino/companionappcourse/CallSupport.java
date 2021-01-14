240e43714b3086fe3bc493a234864e80e48dbea2
Sharvai <sharvai101@gmail.com>
2021-01-14 16:34:55 +0530

    headers updated

--- app/src/main/java/com/droiduino/companionappcourse/CallSupport.java
+++ app/src/main/java/com/droiduino/companionappcourse/CallSupport.java
@@ -1,9 +1,15 @@
 package com.droiduino.companionappcourse;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,9 +24,9 @@ public class CallSupport extends AppCompatActivity {
         // heading.setText("Did you know");
 
         //APP BAR PROPERTIES
-        // getSupportActionBar().hide(); // hides appbar
-        getSupportActionBar().setTitle("Call Support");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //displays back button on app bar
+        getSupportActionBar().hide(); // hides appbar
+//        getSupportActionBar().setTitle("Call Support");
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //displays back button on app bar
 
         //get the spinner from the xml.
         Spinner productnamespinner = findViewById(R.id.productnamespinner);
@@ -31,5 +37,69 @@ public class CallSupport extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         //set the spinners adapter to the previously created one.
         productnamespinner.setAdapter(adapter);
+
+        Session session;//global variable
+        session = new Session(getApplicationContext());
+        String name = session.getname();
+        String id = session.getid();
+
+        TinyDB tinydb = new TinyDB(getApplicationContext());
+        final ArrayList<String> allusers = tinydb.getListString("allusers");
+        System.out.println("ALLUSERSS"+allusers);
+
+        final TextView homeFragmentUsername = (TextView)findViewById(R.id.homeFragmentUsername);
+        homeFragmentUsername.setText(name);
+        homeFragmentUsername.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String allusernames[] = new String[allusers.size()/2];
+                final String alluserids[] = new String[allusers.size()/2];
+                int counter=0;
+                int id_counter=0;
+                for(int i=0;i<allusers.size();i++){
+                    if(i%2!=0){
+                        allusernames[counter]=allusers.get(i);
+                        counter+=1;
+                    }else{
+                        alluserids[id_counter]=allusers.get(i);
+                        id_counter+=1;
+                    }
+                }
+
+                final AlertDialog.Builder builder = new AlertDialog.Builder(CallSupport.this);
+                builder.setTitle("Choose user");
+                builder.setItems(allusernames, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // the user clicked on allusernames[which]
+                        Session session;//global variable
+                        session = new Session(getApplicationContext());
+                        session.setname(allusernames[which]);
+                        session.setid(alluserids[which]);
+
+                        session.settemperature(96); //setting it to default 96F
+                        Fever f = new Fever();
+                        String fever = f.findfever(96);
+                        session.setfever(fever);
+
+                        dialog.dismiss();
+
+                        // reloading activity
+                        finish();
+                        startActivity(getIntent());
+                    }
+                });
+                builder.show();
+            }
+        });
+
+        ImageView doctorbackbtn = findViewById(R.id.doctorbackbtn);
+        doctorbackbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // This is the code to move to another screen
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/droiduino/companionappcourse/Doctor.java
+++ app/src/main/java/com/droiduino/companionappcourse/Doctor.java
@@ -1,8 +1,19 @@
 package com.droiduino.companionappcourse;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class Doctor extends AppCompatActivity {
 
@@ -15,9 +26,73 @@ public class Doctor extends AppCompatActivity {
         // heading.setText("Did you know");
 
         //APP BAR PROPERTIES
-        // getSupportActionBar().hide(); // hides appbar
-        getSupportActionBar().setTitle("DIAGNOSIS");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //displays back button on app bar
+         getSupportActionBar().hide(); // hides appbar
+//        getSupportActionBar().setTitle("DIAGNOSIS");
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(false); //displays back button on app bar
+
+        Session session;//global variable
+        session = new Session(getApplicationContext());
+        String name = session.getname();
+        String id = session.getid();
+
+        TinyDB tinydb = new TinyDB(getApplicationContext());
+        final ArrayList<String> allusers = tinydb.getListString("allusers");
+        System.out.println("ALLUSERSS"+allusers);
+
+        final TextView homeFragmentUsername = (TextView)findViewById(R.id.homeFragmentUsername);
+        homeFragmentUsername.setText(name);
+        homeFragmentUsername.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String allusernames[] = new String[allusers.size()/2];
+                final String alluserids[] = new String[allusers.size()/2];
+                int counter=0;
+                int id_counter=0;
+                for(int i=0;i<allusers.size();i++){
+                    if(i%2!=0){
+                        allusernames[counter]=allusers.get(i);
+                        counter+=1;
+                    }else{
+                        alluserids[id_counter]=allusers.get(i);
+                        id_counter+=1;
+                    }
+                }
+
+                final AlertDialog.Builder builder = new AlertDialog.Builder(Doctor.this);
+                builder.setTitle("Choose user");
+                builder.setItems(allusernames, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // the user clicked on allusernames[which]
+                        Session session;//global variable
+                        session = new Session(getApplicationContext());
+                        session.setname(allusernames[which]);
+                        session.setid(alluserids[which]);
+
+                        session.settemperature(96); //setting it to default 96F
+                        Fever f = new Fever();
+                        String fever = f.findfever(96);
+                        session.setfever(fever);
+
+                        dialog.dismiss();
+
+                        // reloading activity
+                        finish();
+                        startActivity(getIntent());
+                    }
+                });
+                builder.show();
+            }
+        });
+
+        ImageView doctorbackbtn = findViewById(R.id.doctorbackbtn);
+        doctorbackbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // This is the code to move to another screen
+                finish();
+            }
+        });
 
     }
 }
